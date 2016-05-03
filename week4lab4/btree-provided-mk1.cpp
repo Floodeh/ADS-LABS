2e8bd9a88b7d9ce1833494675a84bf0b4c6b5990
Floodeh <adamflood888@hotmail.com>
2016-05-03 11:08:32 +0100

    Added files via upload

--- week4lab4/btree-provided-mk1.cpp
+++ week4lab4/btree-provided-mk1.cpp
@@ -1,1 +1,75 @@
+/*
+ * btree.cpp
+ *
+ *  Created on: 2 Mar 2010
+ *      Author: Mary
+ */
+#include "btree-provided.h"
+#include <iostream>
+#include <cstdlib>//for random functions
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+
+TreeNode::TreeNode(DATATYPE theData)
+:data(theData), rightPtr(NULL), leftPtr(NULL)
+ {
+	
+ }
+
+BinaryTree::BinaryTree()
+:root(NULL)
+ {}
+
+void BinaryTree::add(DATATYPE data)
+{
+	// create the node to attach
+	TreeNode *toAttach = new TreeNode(data);
+
+	//if tree is empty, must put new node here
+	if (!root)
+	{
+		root = toAttach;
+		return;
+	}
+
+	bool attached = false;
+	//start at root
+	TreeNode *possAttachPoint = root;
+
+
+	int randnum;
+	
+	while (!attached)
+	{
+		//choose right or left randomly
+
+		randnum = rand();
+		if (randnum % 2 == 0) //go right
+		{
+			//go right
+			if ( possAttachPoint->rightPtr == NULL)
+			{
+				possAttachPoint->rightPtr = toAttach;
+				attached = true;
+			}
+			else
+				possAttachPoint = possAttachPoint->rightPtr;
+		}
+		else
+		{
+			//go left
+			if (possAttachPoint->leftPtr == NULL)
+			{
+				possAttachPoint->leftPtr = toAttach;
+				attached = true;
+			}
+			else
+				possAttachPoint = possAttachPoint->leftPtr;
+		}
+	}
+}
+
+
 
--- week4lab4/btree-provided.h
+++ week4lab4/btree-provided.h
@@ -1,1 +1,35 @@
+#ifndef BTREE_H_
+#define BTREE_H_
 
+typedef int DATATYPE;
+
+class TreeNode 
+{
+	friend class BinaryTree;
+  public:
+	 TreeNode(DATATYPE theData);
+  private:
+	  TreeNode *leftPtr;
+	  int data;
+	  TreeNode *rightPtr;
+};
+	
+class BinaryTree 
+{
+public:
+		BinaryTree();
+		/*
+		 * add data by travelling randomly 
+		 * right and left in the tree
+		 * until finding a point at which the node 
+		 * can be attached
+		 */ 
+		void add(DATATYPE data); 
+		int height(); //return the height of the tree
+private:
+		int height(TreeNode *n); //recursive call to return the height of tree rooted at n
+		TreeNode *root;
+};
+
+
+#endif
